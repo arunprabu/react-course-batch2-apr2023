def1f3ec73c567a8a6df5ec513fec8e25b132d33
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-04-26 13:22:46 +0530

    updated

--- prime-video-app-apr2023/src/pages/HomePage/HomePage.js
+++ prime-video-app-apr2023/src/pages/HomePage/HomePage.js
@@ -1,34 +1,41 @@
 import LatestVideo from "./LatestVideo/LatestVideo";
+import TrendingVideoList from "./TrendingVideoList/TrendingVideoList";
 
 const HomePage = () => {
   return (
-    <div className="row">
+    <div>
       <h1>Welcome to Home Page</h1>
-      <h2>Let's learn about React's Core Concepts</h2>
+      <h2>Latest Videos | Fn Comp and Props Demo</h2>
 
-      <h2>Latest Videos | Fn Comp and Props</h2>
-      <div className="col-md-3">
-        <LatestVideo
-          title="Fra vs Arg FIFA 2022 Final"
-          thumbnailUrl="https://placehold.co/260x220"
-          views="2.4M"
-          publishedDate="10 Days ago"
-        >
-          Watch highlights of Arg vs Fra FIFA 2022 Final, Qatar
-        </LatestVideo>
+      <div className="row">
+        <div className="col-md-3">
+          <LatestVideo
+            title="Fra vs Arg FIFA 2022 Final"
+            thumbnailUrl="https://placehold.co/260x220"
+            views="2.4M"
+            publishedDate="10 Days ago"
+          >
+            Watch highlights of Arg vs Fra FIFA 2022 Final, Qatar
+          </LatestVideo>
+        </div>
+
+        <div className="col-md-3">
+          <LatestVideo
+            title="CSK vs KKR 2023 IPL"
+            thumbnailUrl="https://placehold.co/260x220"
+            views="1.6M"
+            publishedDate="1 Day ago"
+          >
+            Watch highlights of CSK vs KKR 2023 IPL
+          </LatestVideo>
+        </div>
       </div>
 
-      <div className="col-md-3">
-        <LatestVideo
-          title="CSK vs KKR 2023 IPL"
-          thumbnailUrl="https://placehold.co/260x220"
-          views="1.6M"
-          publishedDate="1 Day ago"
-        >
-          Watch highlights of CSK vs KKR 2023 IPL
-        </LatestVideo>
+      <div className="row mt-5">
+        <hr />
+        <h2>Trending Videos | Class Comp, States and Events Demo</h2>
+        <TrendingVideoList />
       </div>
-
     </div>
   );
 };
