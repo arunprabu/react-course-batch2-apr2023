729af6930d74f2d54fe1346e85f28cc7be74774e
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-07-27 13:22:58 +0530

    updatedd

--- prime-video-app-apr2023/src/pages/ContactUsPage/ContactUsPage.test.js
+++ prime-video-app-apr2023/src/pages/ContactUsPage/ContactUsPage.test.js
@@ -1,6 +1,9 @@
 // AAA
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import ContactUsPage from './ContactUsPage';
+import axios from 'axios';
+
+jest.mock('axios');
 
 // Test suite
 describe('ContactUsPage', () => {
@@ -63,4 +66,90 @@ describe('ContactUsPage', () => {
 
   // TODO: trigger the submit btn click and check whether success msg is displayed or not
   // render + fireEvent + mock resolve + assert
+  // The following 2 ways can be implemented for mocking post. Preferred one is first approach
+  it('has submit button and on click shows success message', async () => {
+    render(
+      <ContactUsPage />
+    );
+
+    // firing events
+    const nameInput = screen.getByLabelText('Name');
+
+    const mockEvenObject = {
+      target: {
+        value: 'abc'
+      }
+    };
+
+    fireEvent.change(nameInput, mockEvenObject);
+
+    // mock resolve
+    const mockResponse = {
+      data: {
+        fullName: 'arun',
+        email: 'a@b.com',
+        phone: '12345',
+        query: 'Would the test spec work?',
+        isSubmitting: false,
+        isSaved: false,
+        isError: false
+      }
+    };
+
+    axios.post.mockResolvedValue(mockResponse);
+
+    const submitBtn = screen.getByRole('button');
+    fireEvent.click(submitBtn);
+
+    // assert
+    expect(await screen.findByText('Saved Successfully!')).toBeInTheDocument();
+  });
+
+  it('submits the form with valid input', async () => {
+    axios.post.mockResolvedValue({
+      data: {
+        fullName: 'a',
+        email: 'a@b.com',
+        phone: '12345',
+        query: 'Would the test spec work?',
+        isSubmitting: false,
+        isSaved: false,
+        isError: false
+      }
+    });
+
+    render(
+      <ContactUsPage />
+    );
+
+    fireEvent.change(screen.getByLabelText('Name'), { target: { value: 'a' } });
+
+    fireEvent.change(screen.getByLabelText('Email Address'), {
+      target: { value: 'a@b.com' }
+    });
+    fireEvent.change(screen.getByLabelText('Phone'), {
+      target: { value: 2727272727 }
+    });
+
+    fireEvent.change(screen.getByLabelText('Query'), {
+      target: { value: 'Great App' }
+    });
+    fireEvent.click(screen.getByText('Submit'));
+    await waitFor(() =>
+      expect(screen.getByText(/Saved Successfully!/i)).toBeInTheDocument()
+    );
+
+    expect(axios.post).toHaveBeenCalledWith(
+      'https://jsonplaceholder.typicode.com/users',
+      {
+        fullName: 'a',
+        email: 'a@b.com',
+        phone: '2727272727',
+        query: 'Great App',
+        isSubmitting: false,
+        isSaved: false,
+        isError: false
+      }
+    )
+  });
 });
