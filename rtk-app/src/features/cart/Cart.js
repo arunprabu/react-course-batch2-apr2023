8b2961ca41a2233f23624a2f9c1a91e57c64e679
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-07-27 11:37:20 +0530

    updated

--- rtk-app/src/features/cart/Cart.js
+++ rtk-app/src/features/cart/Cart.js
@@ -1,54 +1,55 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Cart = () => {
+  const productsState = useSelector((state) => {
+    return state.products;
+  });
+
   return (
     <div className="row">
       <div className="col-xl-12">
         {/* loop thru here */}
-        <div className="card border shadow-none">
-          <div className="card-body">
-            <div className="d-flex align-items-start pb-3">
-              <div className="me-4">
-                <img
-                  src="https://www.bootdey.com/image/380x380/008B8B/000000"
-                  alt=""
-                  className="avatar-lg rounded"
-                  height="100px"
-                />
-              </div>
-              <div className="flex-grow-1 align-self-center overflow-hidden">
-                <div className="col-md-9">
-                  <h5 className="text-truncate font-size-18">
-                    <a href="#" className="text-dark">
-                      Waterproof Mobile Phone{" "}
-                    </a>
-                  </h5>
-                  <p className="text-muted mb-0">
-                    <i className="bx bxs-star text-warning" />
-                    <i className="bx bxs-star text-warning" />
-                    <i className="bx bxs-star text-warning" />
-                    <i className="bx bxs-star text-warning" />
-                    <i className="bx bxs-star-half text-warning" />
-                  </p>
-                  <p className="mb-0 mt-1">
-                    Category : <span className="fw-medium">Fruit</span>
-                  </p>
-                  <div className="mt-3">
-                    <p className="text-muted mb-2">Price</p>
-                    <h5 className="mb-0 mt-2">
-                      <span className="text-muted me-2">
-                        <del className="font-size-16 fw-normal">$500</del>
-                      </span>
-                      $450
-                    </h5>
+        {productsState.cartItems?.map( cartItem => {
+          return (
+            <div className="card border shadow-none" key={cartItem.id}>
+              <div className="card-body">
+                <div className="d-flex align-items-start pb-3">
+                  <div className="me-4">
+                    <img
+                      src={cartItem.image}
+                      alt={cartItem.title}
+                      className="avatar-lg rounded"
+                      height="100px"
+                    />
+                  </div>
+                  <div className="flex-grow-1 align-self-center overflow-hidden">
+                    <div className="col-md-9">
+                      <h5 className="text-truncate font-size-18">
+                        <a href="#" className="text-dark">
+                          {cartItem.title}
+                        </a>
+                      </h5>
+                      <p className="mb-0 mt-1">
+                        Category : <span className="fw-medium">{cartItem.category}</span>
+                      </p>
+                      <div className="mt-3">
+                        <p className="text-muted mb-2">Price</p>
+                        <h5 className="mb-0 mt-2">
+                          <span className="text-muted me-2">
+                            ${cartItem.price}
+                          </span>
+                        </h5>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-
+          );
+        })}
+        
         {/* end card */}
         <div className="row my-4">
           <div className="col-sm-6">
